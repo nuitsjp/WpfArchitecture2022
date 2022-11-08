b9cb1d0fbc3ec3d9e71a76131ea6934c074e6905
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2022-11-08 13:34:02 +0900

    IconPackの追加

---
+++ Source/AdventureWorks.Purchasing/AdventureWorks.Purchasing.View/Control/MenuItemButton.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.IconPacks;
+
+namespace AdventureWorks.Purchasing.View.Control
+{
+    /// <summary>
+    /// MenuItemButton.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MenuItemButton : UserControl
+    {
+        public MenuItemButton()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            nameof(Icon), typeof(PackIconFontAwesomeKind), typeof(MenuItemButton), new PropertyMetadata(default(PackIconFontAwesomeKind)));
+
+        public PackIconFontAwesomeKind Icon
+        {
+            get => (PackIconFontAwesomeKind) GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty MenuLabelProperty = DependencyProperty.Register(
+            nameof(MenuLabel), typeof(string), typeof(MenuItemButton), new PropertyMetadata(default(string)));
+
+        public string MenuLabel
+        {
+            get => (string) GetValue(MenuLabelProperty);
+            set => SetValue(MenuLabelProperty, value);
+        }
+    }
+}
