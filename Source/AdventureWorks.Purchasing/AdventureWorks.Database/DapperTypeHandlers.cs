cad2c106165d6e189b5a64212aeac1c2b6d597c6
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-01-03 09:21:34 +0900

    Add AdventureWorks.Database.

---
+++ Source/AdventureWorks.Purchasing/AdventureWorks.Database/DapperTypeHandlers.cs
@@ -0,0 +1,161 @@
+﻿// ReSharper disable RedundantNameQualifier
+using System.Data;
+using System.Runtime.CompilerServices;
+using Dapper;
+
+namespace AdventureWorks.Database;
+
+
+public class DateTypeHandler : SqlMapper.TypeHandler<Date>
+{
+    public override void SetValue(IDbDataParameter parameter, Date value)
+    {
+        parameter.DbType = DbType.DateTime;
+        parameter.Value = value.AsPrimitive();
+    }
+
+    public override Date Parse(object value)
+    {
+        return new Date((System.DateTime)value);
+    }
+}
+
+
+public class DaysTypeHandler : SqlMapper.TypeHandler<Days>
+{
+    public override void SetValue(IDbDataParameter parameter, Days value)
+    {
+        parameter.DbType = DbType.Int32;
+        parameter.Value = value.AsPrimitive();
+    }
+
+    public override Days Parse(object value)
+    {
+        return new Days((System.Int32)value);
+    }
+}
+
+
+public class DollarTypeHandler : SqlMapper.TypeHandler<Dollar>
+{
+    public override void SetValue(IDbDataParameter parameter, Dollar value)
+    {
+        parameter.DbType = DbType.Decimal;
+        parameter.Value = value.AsPrimitive();
+    }
+
+    public override Dollar Parse(object value)
+    {
+        return new Dollar((System.Decimal)value);
+    }
+}
+
+
+public class DollarPerGramTypeHandler : SqlMapper.TypeHandler<DollarPerGram>
+{
+    public override void SetValue(IDbDataParameter parameter, DollarPerGram value)
+    {
+        parameter.DbType = DbType.Decimal;
+        parameter.Value = value.AsPrimitive();
+    }
+
+    public override DollarPerGram Parse(object value)
+    {
+        return new DollarPerGram((System.Decimal)value);
+    }
+}
+
+
+public class EmployeeIdTypeHandler : SqlMapper.TypeHandler<EmployeeId>
+{
+    public override void SetValue(IDbDataParameter parameter, EmployeeId value)
+    {
+        parameter.DbType = DbType.Int32;
+        parameter.Value = value.AsPrimitive();
+    }
+
+    public override EmployeeId Parse(object value)
+    {
+        return new EmployeeId((System.Int32)value);
+    }
+}
+
+
+public class GramTypeHandler : SqlMapper.TypeHandler<Gram>
+{
+    public override void SetValue(IDbDataParameter parameter, Gram value)
+    {
+        parameter.DbType = DbType.Decimal;
+        parameter.Value = value.AsPrimitive();
+    }
+
+    public override Gram Parse(object value)
+    {
+        return new Gram((System.Decimal)value);
+    }
+}
+
+
+public class ModifiedDateTimeTypeHandler : SqlMapper.TypeHandler<ModifiedDateTime>
+{
+    public override void SetValue(IDbDataParameter parameter, ModifiedDateTime value)
+    {
+        parameter.DbType = DbType.DateTime;
+        parameter.Value = value.AsPrimitive();
+    }
+
+    public override ModifiedDateTime Parse(object value)
+    {
+        return new ModifiedDateTime((System.DateTime)value);
+    }
+}
+
+
+public class RevisionNumberTypeHandler : SqlMapper.TypeHandler<RevisionNumber>
+{
+    public override void SetValue(IDbDataParameter parameter, RevisionNumber value)
+    {
+        parameter.DbType = DbType.Int16;
+        parameter.Value = value.AsPrimitive();
+    }
+
+    public override RevisionNumber Parse(object value)
+    {
+        return new RevisionNumber((System.Int16)value);
+    }
+}
+
+
+public class TaxRateTypeHandler : SqlMapper.TypeHandler<TaxRate>
+{
+    public override void SetValue(IDbDataParameter parameter, TaxRate value)
+    {
+        parameter.DbType = DbType.Decimal;
+        parameter.Value = value.AsPrimitive();
+    }
+
+    public override TaxRate Parse(object value)
+    {
+        return new TaxRate((System.Decimal)value);
+    }
+}
+
+
+class TypeHandlerInitializer
+{
+    #pragma warning disable CA2255
+    [ModuleInitializer]
+    #pragma warning restore CA2255
+    public static void Init()
+    {
+        SqlMapper.AddTypeHandler(new DateTypeHandler());
+        SqlMapper.AddTypeHandler(new DaysTypeHandler());
+        SqlMapper.AddTypeHandler(new DollarTypeHandler());
+        SqlMapper.AddTypeHandler(new DollarPerGramTypeHandler());
+        SqlMapper.AddTypeHandler(new EmployeeIdTypeHandler());
+        SqlMapper.AddTypeHandler(new GramTypeHandler());
+        SqlMapper.AddTypeHandler(new ModifiedDateTimeTypeHandler());
+        SqlMapper.AddTypeHandler(new RevisionNumberTypeHandler());
+        SqlMapper.AddTypeHandler(new TaxRateTypeHandler());
+    }
+}
