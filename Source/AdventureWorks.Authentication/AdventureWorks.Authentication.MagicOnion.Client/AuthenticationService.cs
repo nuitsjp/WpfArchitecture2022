02825a9b3865b3f1de4b326dd77ec8f511c53665
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-02-28 14:24:15 +0900

    認証処理のコメント追加

--- Source/AdventureWorks.Authentication/AdventureWorks.Authentication.MagicOnion.Client/AuthenticationService.cs
+++ Source/AdventureWorks.Authentication/AdventureWorks.Authentication.MagicOnion.Client/AuthenticationService.cs
@@ -1,6 +1,8 @@
 ﻿using AdventureWorks.MagicOnion;
+using Grpc.Core;
 using Grpc.Net.Client;
 using MagicOnion.Client;
+using System.Net;
 
 namespace AdventureWorks.Authentication.MagicOnion.Client;
 
@@ -35,9 +37,12 @@ public class AuthenticationService : IAuthenticationService
         // このコードはクライアントからしか呼ばれないため、そこまで厳密にする必要がないと判断し
         // ここでは警告を抑制する。
 #pragma warning disable CA1416
+        // 本当はAzure Active Directory認証などを用いたいが、サンプルとして動作させるさいに
+        // なんらかの認証サーバーを用意するのは困難なので、ここではOSのユーザーを取得して利用する。
+        // 実際、とくに公開APIではセキュリティ的に問題となるため、下記を参照していずれかの放棄を選択する。
+        // https://learn.microsoft.com/ja-jp/aspnet/core/grpc/authn-and-authz?view=aspnetcore-7.0
         var userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
 #pragma warning restore CA1416
-
         var client = MagicOnionClient.Create<IAuthenticationServiceServer>(GrpcChannel.ForAddress(_config.Address));
         _currentEmployee = await client.GetEmployeeAsync(new LoginId(userName));
 
