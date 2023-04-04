74cc34b47588162c2042fea77be6ad0cda8fe084
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-04-04 20:32:48 +0900

    フォルダー構成の変更

---
+++ Source/AdventureWorks.MagicOnion/Initializer.cs
@@ -0,0 +1,15 @@
+﻿using AdventureWorks.Hosting;
+using AdventureWorks.Hosting.MagicOnion;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdventureWorks.MagicOnion;
+
+public static class Initializer
+{
+    public static void Initialize(IMagicOnionApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton(_ => builder.Configuration.GetRequiredSection("MagicOnion").Get<MagicOnionConfig>()!);
+        builder.AddFormatterResolver(CustomResolver.Instance);
+    }
+}
