8c162ce8a7e0744c20d22030a44da5eb882d4934
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-04-08 17:57:54 +0900

    不要なSerilog初期化メソッドの削除

--- Source/AdventureWorks.Hosting.Wpf/WpfApplicationBuilder.cs
+++ Source/AdventureWorks.Hosting.Wpf/WpfApplicationBuilder.cs
@@ -35,7 +35,6 @@ public class WpfApplicationBuilder<TApplication, TWindow> : IMagicOnionApplicati
     {
         // Serilogの初期化
         Logging.Serilog.Hosting.Wpf.Initializer.InitializeAsync(applicationName).Wait();
-        InitializeSerilog(Configuration, applicationName);
         LoggingAspect.Logger = new ViewModelLogger();
 
         // MagicOnionの初期化
@@ -102,58 +101,6 @@ public class WpfApplicationBuilder<TApplication, TWindow> : IMagicOnionApplicati
     public static WpfApplicationBuilder<TApplication, TWindow> CreateBuilder()
     {
         return new(KamishibaiApplication<TApplication, TWindow>.CreateBuilder());
-    }
-
-    public static void InitializeSerilog(IConfiguration configuration, string applicationName)
-    {
-        var connectionString = ConnectionStringProvider.Resolve("sa", "P@ssw0rd!");
-        var minimumLevel = GetMinimumLevel(connectionString, applicationName);
-        var settingString = Properties.Resources.Wpf
-            .Replace("%ConnectionString%", connectionString)
-            .Replace("%MinimumLevel%", minimumLevel)
-            .Replace("%ApplicationName%", applicationName);
-        using var settings = new MemoryStream(Encoding.UTF8.GetBytes(settingString));
-        var cc = new ConfigurationBuilder()
-            .AddJsonStream(settings)
-            .Build();
-
-        Log.Logger = new LoggerConfiguration()
-            .ReadFrom.Configuration(cc)
-#if DEBUG
-            .WriteTo.Debug()
-#endif
-            .CreateLogger();
-    }
-
-    // ReSharper disable UnusedParameter.Local
-    private static string GetMinimumLevel(string connectionString, string applicationName)
-    {
-#if DEBUG
-        return "Debug";
-#else
-            const string defaultMinimumLevel = "Information";
-            try
-            {
-                using var connection = new SqlConnection(connectionString);
-                connection.Open();
-
-                return connection.QuerySingleOrDefault<string>(@"
-select
-	MinimumLevel
-from
-	[dbo].[LogSettings]
-where
-	ApplicationName = @ApplicationName",
-                    new
-                    {
-                        ApplicationName = applicationName
-                    }) ?? defaultMinimumLevel;
-            }
-            catch
-            {
-                return defaultMinimumLevel;
-            }
-#endif
     }
 }
 
