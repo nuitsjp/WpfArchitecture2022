0aa6934659ca5735f11f5b8c2f1f5547dfbb98e9
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-04-24 15:01:21 +0900

    SerilogConfigRepositoryClientのプロジェクト移動

--- Source/AdventureWorks.Logging.Serilog.MagicOnion.Client/SerilogConfigRepositoryClient.cs
+++ Source/AdventureWorks.Logging.Serilog.MagicOnion.Client/SerilogConfigRepositoryClient.cs
@@ -1,25 +1,25 @@
-﻿//using AdventureWorks.MagicOnion.Client;
+﻿using AdventureWorks.MagicOnion.Client;
 
-//namespace AdventureWorks.Logging.Serilog.MagicOnion.Client;
+namespace AdventureWorks.Logging.Serilog.MagicOnion.Client;
 
-//public class SerilogConfigRepositoryClient : ISerilogConfigRepository
-//{
-//    private readonly IMagicOnionClientFactory _clientFactory;
+public class SerilogConfigRepositoryClient : ISerilogConfigRepository
+{
+    private readonly IMagicOnionClientFactory _clientFactory;
 
 
-//    public SerilogConfigRepositoryClient(IMagicOnionClientFactory clientFactory)
-//    {
-//        _clientFactory = clientFactory;
-//    }
+    public SerilogConfigRepositoryClient(IMagicOnionClientFactory clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
 
-//    public Task<SerilogConfig> GetServerSerilogConfigAsync(ApplicationName applicationName)
-//    {
-//        throw new NotImplementedException();
-//    }
+    public Task<SerilogConfig> GetServerSerilogConfigAsync(ApplicationName applicationName)
+    {
+        throw new NotImplementedException();
+    }
 
-//    public async Task<SerilogConfig> GetClientSerilogConfigAsync(ApplicationName applicationName)
-//    {
-//        var service = _clientFactory.Create<ISerilogConfigService>();
-//        return await service.GetServerSerilogConfigAsync(applicationName.Value);
-//    }
-//}
+    public async Task<SerilogConfig> GetClientSerilogConfigAsync(ApplicationName applicationName)
+    {
+        var service = _clientFactory.Create<ISerilogConfigService>();
+        return await service.GetServerSerilogConfigAsync(applicationName.Value);
+    }
+}
